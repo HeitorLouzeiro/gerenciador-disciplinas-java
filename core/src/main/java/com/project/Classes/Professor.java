6ce9f0e68c684a347b9b4beb313e4ef2cf89696b
Claudemiro-Nogueira <95505494+Claudemiro-Nogueira@users.noreply.github.com>
2023-11-09 16:16:36 -0300

    Criacao classe professor  (#6)
    * Criacao classe Professor com os metodos get e set
    
    * Classe funcionario com get e set

---
+++ core/src/main/java/com/project/Classes/Professor.java
@@ -0,0 +1,27 @@
+package com.project.Classes;
+
+class Professor extends Funcionario{
+    protected String titulacao;
+    protected String areaAtuacao;
+
+    public Professor(String titulacao, String areaAtuacao){
+        this.titulacao = titulacao;
+        this.areaAtuacao = areaAtuacao;
+    }
+    
+    public String getTitulacao() {
+        return titulacao;
+    }
+
+    public void setTitulacao(String titulacao) {
+        this.titulacao = titulacao;
+    }
+
+    public String getAreaAtuacao() {
+        return areaAtuacao;
+    }
+
+    public void setAreaAtuacao(String areaAtuacao) {
+        this.areaAtuacao = areaAtuacao;
+    }
+}
